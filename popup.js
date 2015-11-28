498a2958c197c9d9c6de7f21f4bdf8a858661bd4
Manohar Prabhu <manoharagent13@gmail.com>
2015-11-29 02:20:36 +0530

    Enforced jslint rules

--- popup.js
+++ popup.js
@@ -1,51 +1,55 @@
-/* global chrome */
-window.onload = function(){
-	populateBlockedList();
-}
-$(document).ready(function(){
-	$('#submit-item').on('click',function(){
-		if($('#block-item').val().length === 0){
-			return false;
-		}
-		chrome.storage.local.get({'blockedWords':[]}, function (result) {
-			var value = $('#block-item').val();
-			result.blockedWords.push(value);
-			chrome.storage.local.set(result);
-			populateBlockedList();	
-		});
-	});
-	$('#block-item').keydown(function(event){
-		if(event.keyCode === 13){
-			$('#submit-item').click();
-		}
-	});
-	
-	$('#clear-all').on('click',function(){
-		chrome.storage.local.clear();
-		populateBlockedList();
-	})
-});
+/*jslint browser: true*/
+/*global $, jQuery, chrome*/
+(function () {
+    'use strict';
+    var populateBlockedList = function () {
+        $('#blocked-list').empty();
+        $('#block-item').val('');
+        chrome.storage.local.get({'blockedWords': []}, function (result) {
+            result.blockedWords.forEach(function (item) {
+                var oneItem = $('<li>')
+                    .text(item)
+                    .attr('href', '#')
+                    .on('click', function () {
+                        var clickedItem = $(this).text();
+                        chrome.storage.local.get({'blockedWords': []}, function (result) {
+                            var index = result.blockedWords.indexOf(clickedItem);
+                            if (index > -1) {
+                                result.blockedWords.splice(index, 1);
+                            }
+                            chrome.storage.local.set(result);
+                            populateBlockedList();
+                        });
+                    });
+                $('#blocked-list').append(oneItem);
+            });
+        });
+    };
+    window.onload = function () {
+        populateBlockedList();
+    };
+    $(document).ready(function () {
+        $('#submit-item').on('click', function () {
+            if ($('#block-item').val().length === 0) {
+                return false;
+            }
+            chrome.storage.local.get({'blockedWords': []}, function (result) {
+                var value = $('#block-item').val();
+                result.blockedWords.push(value);
+                chrome.storage.local.set(result);
+                populateBlockedList();
+            });
+        });
+        $('#block-item').keydown(function (event) {
+            if (event.keyCode === 13) {
+                $('#submit-item').click();
+            }
+        });
 
-var populateBlockedList = function () {
-	$('#blocked-list').empty();
-	$('#block-item').val('');
-	chrome.storage.local.get({'blockedWords':[]}, function (result) {
-		result.blockedWords.forEach(function(item){
-			var oneItem = $('<li>')
-					.text(item)
-					.attr('href','#')
-					.on('click',function(){
-						var clickedItem = $(this).text();
-						chrome.storage.local.get({'blockedWords':[]}, function (result) {
-							var index = result.blockedWords.indexOf(clickedItem); 
-							if (index > -1) {
- 							   result.blockedWords.splice(index, 1);
-							}
-							chrome.storage.local.set(result);
-							populateBlockedList();
-						});
-					});
-			$('#blocked-list').append(oneItem);
-		});
+        $('#clear-all').on('click', function () {
+            chrome.storage.local.clear();
+            populateBlockedList();
+        });
     });
-}
+
+}());
